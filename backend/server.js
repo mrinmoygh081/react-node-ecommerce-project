298aa55ae3b18a1487a5dbca34011c3b266635cb
mrinmoygh081 <mrinmoygh081@gmail.com>
2021-06-04 11:41:06 +0530

    backend server set up

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import data from "./data.js";
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get("/api/products", (req, res) => {
+  res.send(data.products);
+});
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
