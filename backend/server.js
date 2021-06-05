76b53998ccb8316bf16a907677fba7de914ff5b8
mrinmoygh081 <mrinmoygh081@gmail.com>
2021-06-06 04:00:58 +0530

    Load Product from Backend

--- backend/server.js
+++ backend/server.js
@@ -11,5 +11,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`server listening at http://localhost:${port}`);
 });
