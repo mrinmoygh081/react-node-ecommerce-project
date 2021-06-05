d4d78e56a2d289f77507d27a46ea960afa161284
mrinmoygh081 <mrinmoygh081@gmail.com>
2021-06-06 04:02:06 +0530

    front change of Load Product from backend

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,17 +1,43 @@
-import React from "react";
-// import ProductItem from "./ProductItem";
-import data from "../data";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+// import data from "../data";
 import ProductItem from "./ProductItem";
+import MessageBox from "./MessageBox";
+import LoadingBox from "./LoadingBox";
 
 function Products() {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const { data } = await axios.get("/api/products");
+        setLoading(false);
+        setProducts(data);
+      } catch (err) {
+        setError(err.message);
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <main>
       <div>
-        <div className="row center">
-          {data.products.map((product) => {
-            return <ProductItem key={product._id} product={product} />;
-          })}
-        </div>
+        {loading ? (
+          <LoadingBox />
+        ) : error ? (
+          <MessageBox>{error}</MessageBox>
+        ) : (
+          <div className="row center">
+            {products.map((product) => {
+              return <ProductItem key={product._id} product={product} />;
+            })}
+          </div>
+        )}
       </div>
     </main>
   );
