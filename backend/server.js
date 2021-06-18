88aa508acf3d805fc0cafdad2dff3c508f8e65b4
mrinmoygh081 <mrinmoygh081@gmail.com>
2021-06-18 17:50:35 +0530

    final commit for now

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,14 @@ const port = process.env.PORT || 5000;
 app.get("/api/products", (req, res) => {
   res.send(data.products);
 });
+app.get("/api/products/:id", (req, res) => {
+  const product = data.products.find((x) => x._id === req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "Product not found" });
+  }
+});
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
